389449db16ba1293039c432a06989804f0629af7
Loba <ajidevelop@gmail.com>
2022-02-04 20:28:10 -0600

    Changed websocket library and made working websocket request

--- app.py
+++ app.py
@@ -1,30 +1,17 @@
-import json
 from flask import Flask
-from take_profit import StopLossTakeProfit
-import asyncio
-import threading
-
+from take_profit import WebsocketClient
 app = Flask(__name__)
 
-sltp = StopLossTakeProfit()
-# t1 = threading.Thread(target=sltp., name='coin-ticker')
-# t1.start()
 
 @app.get('/get_orders')
 def get_orders():
-    return sltp.sltp
+    pass
 
 
 @app.post('/new_order')
 def new_order(coin, curr, sl, tp):
-    sltp.sltp[f'{coin}-{curr}'] = {
-        'stop_loss': sl,
-        'take_profit': tp
-    }
-    sltp.json_to_file()
+    pass
 
-    return json.load
 
-print('works')
-# t1.join()
-sltp.stop_loss()
+client = WebsocketClient()
+client.start()
--- take_profit.py
+++ take_profit.py
@@ -1,9 +1,12 @@
 import asyncio
 import os
 import json
+import time
+
+from threading import Thread
 from helpers.auth import CoinbaseAuth
 from requests import request
-from websockets import connect
+from websocket import create_connection
 from dotenv import load_dotenv
 
 load_dotenv('.env')
@@ -15,6 +18,90 @@ headers = CoinbaseAuth(os.getenv('key'), os.getenv('secret'), os.getenv('passphr
 WS_URI = 'wss://ws-feed.exchange.coinbase.com'
 # API_URI = 'https://api.exchange.coinbase.com/orders'
 API_URI = 'https://api-public.sandbox.exchange.coinbase.com'
+
+
+class WebsocketClient(object):
+    def __init__(self):
+        self.url = 'wss://ws-feed.exchange.coinbase.com'
+        self.products = json.load(open('sltp.json', 'r'))
+        self.channels = None
+        self.stop = True
+        self.ws = None
+        self.thread = None
+        self.keep_alive = None
+        self.error = None
+        self.prices = dict()
+
+    def start(self):
+        def _go():
+            self._connect()
+            self._listen()
+            self._disconnect()
+
+        self.stop = False
+        self.keep_alive = Thread(target=self._keep_alive)
+        self.thread = Thread(target=_go())
+        self.thread.start()
+
+    def _connect(self):
+        self.products = [f'{coin_pair}' for coin_pair in self.products.keys()]
+
+        if self.channels is None:
+            self.channels = ['ticker']
+
+        params = {'type': 'subscribe', 'product_ids': self.products, 'channels': self.channels}
+
+        self.ws = create_connection(self.url)
+        self.ws.send(json.dumps(params))
+
+    def _keep_alive(self, interval=30):
+        while self.ws.connected:
+            self.ws.ping('keepalive')
+            time.sleep(interval)
+
+    def _listen(self):
+        self.keep_alive.start()
+        while not self.stop:
+            try:
+                data = self.ws.recv()
+                msg = json.loads(data)
+            except ValueError as e:
+                self.on_error(e)
+            else:
+                self.on_message(msg)
+
+    def _disconnect(self):
+        try:
+            if self.ws:
+                self.ws.close()
+        except Exception as e:
+            self.on_error(e)
+        finally:
+            self.keep_alive.join()
+
+        self.on_close()
+
+    def close(self):
+        self.stop = True
+        self._disconnect()
+        self.thread.join()
+
+    @staticmethod
+    def on_open():
+        print("-- Subscribed! --\n")
+
+    @staticmethod
+    def on_close():
+        print("\n-- Socket Closed --")
+
+    @staticmethod
+    def on_message(msg):
+        print(msg)
+
+    def on_error(self, e, data=None):
+        self.error = e
+        self.stop = True
+        print('{} - data: {}'.format(e, data))
 
 
 class StopLossTakeProfit:
